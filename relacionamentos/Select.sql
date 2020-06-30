9af47df09b7c633b64feefec12a12900fc37f3b1
Douglas.Santos <douglas.santos@softplan.com.br>
2020-06-30 15:25:44 -0300

    Atualização projeto teste WebdriverIO + Cucumber + Selenium

--- relacionamentos/Select.sql
+++
@@ -1,7 +0,0 @@
-select * from person p
-	join country c
-		on p.Country_idCountry = c.idCountry
-	join Languages l
-		on l.idLanguages = p.Languages_idLanguages
-	join Skills s
-		on s.idSkills = p.Skill_idSkills;
